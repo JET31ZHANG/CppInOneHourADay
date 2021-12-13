8b3eb93d45e8dde101180c0e0e9e3f758678fe2e
JET31ZHANG <66536823+JET31ZHANG@users.noreply.github.com>
2021-12-13 22:26:17 +0800

    upload example files left in lesson7

---
+++ lesson7/exp_7_10.cpp
@@ -0,0 +1,23 @@
+/*
+    Using an inline function that doubles an integer
+*/
+#include <iostream>
+using namespace std;
+
+// define an inline function that doubles
+inline long DoubleNum(int inputNum)
+{
+    return inputNum * 2;
+}
+
+int main()
+{
+    cout << "Enter an integer: ";
+    int inputNum = 0;
+    cin >> inputNum;
+
+    // Call inline function
+    cout << "Double is: " << DoubleNum(inputNum) << endl;
+
+    return 0;
+}
