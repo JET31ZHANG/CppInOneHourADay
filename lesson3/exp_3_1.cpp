0a35098eb055323b87037b1de5775fd3aa63a97a
JET31ZHANG <66536823+JET31ZHANG@users.noreply.github.com>
2021-12-13 22:10:44 +0800

    Create exp_3_1.cpp

---
+++ lesson3/exp_3_1.cpp
@@ -0,0 +1,28 @@
+/*
+    Use variables to store numbers and the result 
+    of their multiplication
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "This program will help you multiply two numbers" << endl;
+
+    cout << "Enter the first number: ";
+    int firstNumber = 0;
+    cin >> firstNumber;
+
+    cout << "Enter the second number: ";
+    int secondNumber = 0;
+    cin >> secondNumber;
+
+    // Multiply two numbers, store result in a variable
+    int multiplicationResult = firstNumber * secondNumber;
+
+    // Display result
+    cout << firstNumber << " x " << secondNumber;
+    cout << " = " << multiplicationResult << endl;
+
+    return 0;
+}
