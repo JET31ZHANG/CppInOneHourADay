91e5990cac6ea964513f7d2906c851535eb80d08
JET31ZHANG <66536823+JET31ZHANG@users.noreply.github.com>
2021-12-13 22:08:01 +0800

    Create exp_2_1.cpp

---
+++ lesson2/exp_2_1.cpp
@@ -0,0 +1,17 @@
+/* 
+  HelloWorldAnalysis.cpp: analyze a simple C++ program 
+*/
+// Preprocessor directive thqt includes header iostream
+
+#include <iostream>
+
+// Start of your program: function block main()
+
+int main()
+{
+    /* Write to the screen */
+    std::cout << "Hello World!" << std::endl;
+
+    // Return a value to the OS
+    return 0;
+}
