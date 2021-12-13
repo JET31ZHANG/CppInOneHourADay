a608589d754e509bbfdc2d1884958f0df8ecbbd5
JET31ZHANG <66536823+JET31ZHANG@users.noreply.github.com>
2021-12-13 22:09:02 +0800

    upload example file left in lesson2

---
+++ lesson2/exp_2_6.cpp
@@ -0,0 +1,26 @@
+/*
+     use cin and cout to display  input of number and text by user 
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // Declare a variable to store an integer
+    int inputNumber;
+
+    cout << "Enter an integer: ";
+
+    // store integer given user input
+    cin >> inputNumber;
+
+    // The same with text i.e. string data
+    cout << "Enter your name: ";
+    string inputName;
+    cin >> inputName;
+
+    cout << inputName << " entered " << inputNumber << endl;
+
+    return 0;
+}
