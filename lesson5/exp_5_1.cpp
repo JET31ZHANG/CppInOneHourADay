2bfd78beb49abac6262acfbc6a269bf1ab015dac
JET31ZHANG <66536823+JET31ZHANG@users.noreply.github.com>
2021-12-13 22:14:56 +0800

    Create exp_5_1.cpp

---
+++ lesson5/exp_5_1.cpp
@@ -0,0 +1,18 @@
+// Demonstrate Arithmetic Operators on integers input by the user
+#include <iostream>
+using namespace std;
+int main() 
+{
+    cout << "Enter two integers: " << endl;
+    int num1 = 0, num2 = 0;
+    cin >> num1;
+    cin >> num2;
+
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+    cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+    cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+    cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+    cout << num1 << " % " << num2 << " = " << num1 % num2 << endl;
+
+    return 0;
+}
