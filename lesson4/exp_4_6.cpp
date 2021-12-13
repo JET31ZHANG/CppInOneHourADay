ba6617501fd51b6f138b22d91063af0af12df26f
JET31ZHANG <66536823+JET31ZHANG@users.noreply.github.com>
2021-12-13 22:13:57 +0800

    upload the example files left in lesson4

---
+++ lesson4/exp_4_6.cpp
@@ -0,0 +1,15 @@
+/*
+    A risky applicaiton using C-style strings and user input
+*/
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+    cout << "Enter a word NOT longer than 20 characters: " << endl;
+    char userInput [21] = {'\0'};
+    cin >> userInput;
+
+    cout << "Length of your input was: " << strlen(userInput) << endl;
+    return 0;
+}
